53772ee4f0e71d4781a23af7f0e75d387959a034
Stefan Daschek <stefan@die-antwort.eu>
2016-08-05 15:45:52 +0200

    fetch-upstreams: Skip submodules without upstream remote.
    Until now, the script aborted in this case.

--- fetch-upstreams.sh
+++ fetch-upstreams.sh
@@ -2,5 +2,5 @@
 
 cd ~/Library/Application\ Support/Avian/Bundles || exit 1
 
-git submodule foreach "git fetch upstream && git log head..upstream/master | cat && echo"
+git submodule foreach "git remote | grep -q upstream && (git fetch upstream && git log head..upstream/master | cat) || echo 'WARNING: No remote named \"upstream\", skipping!'; echo"
 
