63b9107fff720eb22fe90303cc13dc64bad6af29
Stefan Daschek <stefan@die-antwort.eu>
2016-10-20 19:06:52 +0200

    Update directory name for current TextMate releases.

--- fetch-upstreams.sh
+++ fetch-upstreams.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ~/Library/Application\ Support/Avian/Bundles || exit 1
+cd ~/Library/Application\ Support/TextMate/Bundles || exit 1
 
 git submodule foreach "git remote | grep -q upstream && (git fetch upstream && git log head..upstream/master | cat) || echo 'WARNING: No remote named \"upstream\", skipping!'; echo"
 
